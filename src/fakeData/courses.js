12042af03205933bfff700e9f6336ea3bca46fe9
Moonjarin <moonjarintasu@gmail.com>
2020-08-28 00:31:23 +0600

    matro hoilo

---
+++ src/fakeData/courses.js
@@ -0,0 +1,71 @@
+var courses=[
+    {
+        name: 'cooking',
+        price: 1500,
+    },
+    {
+        name: 'games',
+        price: 1500,
+    },
+    {
+        name: 'anatomy',
+        price: 3000,
+    },
+    {
+        name: 'chemistry',
+        price: 2500,
+    },
+    {
+        name: 'snake language',
+        price: 1500,
+    },
+    {
+        name: 'poison',
+        price: 1000,
+    },
+    {
+        name: 'wand',
+        price: 1000,
+    },
+    {
+        name: 'dark art',
+        price: 1000,
+    },
+    {
+        name: 'invisibility',
+        price: 1100,
+    },
+    {
+        name: 'bounty',
+        price: 4500,
+    },
+    {
+        name: 'auror',
+        price: 2500,
+    },
+    {
+        name: 'spell',
+        price: 500,
+    },
+    {
+        name: 'chess',
+        price: 1500,
+    },
+    {
+        name: 'calligraphy',
+        price: 1500,
+    },
+    {
+        name: 'herbelogy',
+        price: 1500,
+    },
+    {
+        name: 'broomstick',
+        price: 1000,
+    },
+    {
+        name: 'dark magic',
+        price: 1500,
+    }
+]
+export default courses;
